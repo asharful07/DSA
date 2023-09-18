22bc8b75f0c983c4e3172318536b7fe6f90c0dd2
asharful07 <asharfulkhan202@gmail.com>
2023-09-18 11:39:47 +0530

    Question_01

---
+++ src/DSA/Pattern_Question/Question_01.java
@@ -0,0 +1,19 @@
+
+/*       Print the Pattern
+
+          * * * * *
+          * * * * *
+          * * * * *            */
+
+
+package DSA.Pattern_Question;
+public class Question_01 {
+    public static void main(String[] args) {
+        for(int r=1;r<4;r++){
+            for(int c=1;c<6;c++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
