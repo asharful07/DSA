63a6d8a0bdddff86360997c9253582993bcf6abe
asharful07 <asharfulkhan202@gmail.com>
2023-11-16 23:50:46 +0530

    Question_17

---
+++ src/DSA/Arrays_Question/Question_17.java
@@ -0,0 +1,22 @@
+
+// Java program to find missing Number
+
+package DSA.Arrays_Question;
+public class Question_17 {
+
+        public static int getMissingNo(int[] nums, int n)
+        {
+            int sum = ((n + 1) * (n + 2)) / 2;
+            for (int i = 0; i < n; i++)
+                sum -= nums[i];
+            return sum;
+        }
+        public static void main(String[] args)
+        {
+            int[] arr = { 1, 2, 3,4,6 };
+            int N = arr.length;
+            System.out.println(getMissingNo(arr, N));
+        }
+    }
+
+
