d084561b9b78c0c31a5481634a9aa3c7297e9caf
asharful07 <asharfulkhan202@gmail.com>
2023-11-20 19:40:36 +0530

    Question_08

---
+++ src/DSA/Pattern_Question/Question_08.java
@@ -0,0 +1,46 @@
+
+
+/*
+print the pattern
+
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+0 1 0 1 0 1
+
+
+*/
+
+
+package DSA.Pattern_Question;
+
+public class Question_08 {
+
+
+        public static void printPattern(int n)
+        {
+            int i, j;
+            for (i = 1; i <= n; i++) {
+                for (j = 1; j <= i; j++) {
+
+                    if ((i + j) % 2 == 0) {
+                        System.out.print(1 + " ");
+                    }
+
+                    else {
+                        System.out.print(0 + " ");
+                    }
+                }
+
+
+                System.out.println();
+            }
+        }
+        public static void main(String[] args)
+        {
+            int n = 6;
+            printPattern(n);
+        }
+    }
