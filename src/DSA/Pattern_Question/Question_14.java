24321dc0b92b1aab461bec11ecb9e859c6dddd8e
asharful07 <asharfulkhan202@gmail.com>
2023-11-26 18:53:36 +0530

    Question_14

---
+++ src/DSA/Pattern_Question/Question_14.java
@@ -0,0 +1,47 @@
+/*
+ print the pattern:
+
+             *
+            * *
+           *   *
+          *     *
+         *       *
+        ***********
+
+ */
+
+
+
+
+
+package DSA.Pattern_Question;
+public class Question_14 {
+        public static void printPattern(int n)
+        {
+            int i, j, k;
+
+            for (i = 1; i <= n; i++) {
+
+                for (j = i; j < n; j++) {
+                    System.out.print(" ");
+                }
+
+                for (k = 1; k <= (2 * i - 1); k++) {
+                    if (k == 1 || i == n || k == (2 * i - 1)) {
+                        System.out.print("*");
+                    }
+                    else {
+                        System.out.print(" ");
+                    }
+                }
+
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args)
+        {
+            int n = 6;
+            printPattern(n);
+        }
+    }
