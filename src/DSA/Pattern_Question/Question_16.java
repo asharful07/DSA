4f648a80bf3df6211b07408f6ceefd5cf15e9e41
asharful07 <asharfulkhan202@gmail.com>
2023-11-28 17:18:47 +0530

    Question_16

---
+++ src/DSA/Pattern_Question/Question_16.java
@@ -0,0 +1,55 @@
+/*
+print the pattern :
+
+                   *
+                  * *
+                 *   *
+                *     *
+               *       *
+              *         *
+               *       *
+                *     *
+                 *   *
+                  * *
+                   *
+ */
+
+package DSA.Pattern_Question;
+public class Question_16 {
+        public static void printPattern(int n)
+        {
+            int i, j;
+            int num = 1;
+            for (i = 1; i <= n; i++) {
+                for (j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++) {
+                    if (j == 1 || j == 2*i-1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+
+            for (i = n-1; i >= 1; i--) {
+                for (j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (j = 1; j <= 2 * i - 1; j++) {
+                    if (j == 1 || j == 2*i-1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args)
+        {
+            int n = 6;
+            printPattern(n);
+        }
+    }
